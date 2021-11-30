8d58ff6d90d8f746302b07eac91b32109406ee50
jmgalvis <jmgalviis@gmail.com>
2021-11-30 05:48:52 -0500

    Se prepara configuraciones para deploy

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from decouple import config
 
 
 class Config:
@@ -9,19 +6,19 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    app_debug = os.environ['APP_DEBUG']
-    connection = os.environ['DB_CONNECTION']
-    db_host = os.environ['DB_HOST']
-    db_database = os.environ['DB_DATABASE']
-    db_username = os.environ['DB_USERNAME']
-    db_password = os.environ['DB_PASSWORD']
-
-    DEBUG = app_debug
-    SQLALCHEMY_DATABASE_URI = f'{connection}://{db_username}:{db_password}@{db_host}/{db_database}'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='postgres::/localhost/postgres')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SHOW_SQLALCHEMY_LOG_MESSAGES = False
 
 
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='postgres::/localhost/postgres')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 config = {
-    'development': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
 }
